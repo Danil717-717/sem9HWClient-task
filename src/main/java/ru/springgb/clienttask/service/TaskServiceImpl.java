26763fb8c97df482f450a5e249d42d68ef23a79a
danil <danila_monster@mail.ru>
2024-02-14 16:42:41 +0300

    commit8

--- src/main/java/ru/springgb/clienttask/service/TaskServiceImpl.java
+++ src/main/java/ru/springgb/clienttask/service/TaskServiceImpl.java
@@ -6,7 +6,7 @@ import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-//import ru.springgb.clientexecutor.model.Executor;
+
 import ru.springgb.cliententity.model.Task;
 import ru.springgb.clienttask.logAspect.TrackUserAction;
 
